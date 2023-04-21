74ba486c134dd61f4a73601ce19325776189b168
Ubuntu <azureuser@myVm.bor5b2oam50uzbpaq5qavqriff.cx.internal.cloudapp.net>
2023-04-21 22:47:32 +0000

    update a setup.sh

---
+++ setup.sh
@@ -0,0 +1,26 @@
+apt-get update
+apt-get -y install openjdk-8-jdk
+
+apt update
+apt install -y  maven
+
+apt update
+apt install -y software-properties-common
+add-apt-repository ppa:deadsnakes/ppa -y
+apt update
+apt install python3.8 -y
+
+dnf install git-all 
+apt install git-all -y
+
+
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
+
+update-alternatives  --set python3 /usr/bin/python3.8
+
+apt install python3-tk -y
+apt install python3-pip -y
+pip3 install BeautifulSoup4
+pip3 install jsonlines
+
+apt-get install emacs
