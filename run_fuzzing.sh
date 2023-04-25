65995180b1fba29310ca1b41e48c6b47cd5ad6c5
Ubuntu <azureuser@myVmamd2.hj4btkmmjwbexaxnlhsj524bgd.cx.internal.cloudapp.net>
2023-04-25 22:44:23 +0000

    update run_fuzzing, stop resources when fuzzing finishes to avoid conflicts

--- run_fuzzing.sh
+++ run_fuzzing.sh
@@ -13,6 +13,7 @@ fuzzlogDir=${mainDir}/logs/fuzzing_logs
 logDir=${mainDir}/logs/fuzzing_logs/${timeStamp}
 fuzz=${mainDir}/run_tool.py
 report=${mainDir}/report.py
+stopall=${mainDir}/stop_all.sh
 
 exec 3>&1 4>&2
 trap $(exec 2>&4 1>&3) 0 1 2 3
@@ -29,8 +30,19 @@ generate_report(){
     python3 $report $port $service $tool $logDir
 }
 
+clear_state(){
+    sudo fuser -k 27018/tcp
+    sudo fuser -k 8080/tcp
+}
+
+stop_util_docker(){
+    bash -x $stopall
+}
+
 cd $mainDir
+clear_state
 fuzz_tool_service
 generate_report
+stop_util_docker
 
 echo ENDING at $(date)
