972bc0a739963398930e879c446ab678bee823bb
Ubuntu <azureuser@myVmamd2.hj4btkmmjwbexaxnlhsj524bgd.cx.internal.cloudapp.net>
2023-04-25 01:25:32 +0000

    update a script for multiple

---
+++ run_multiple.sh
@@ -0,0 +1,38 @@
+baseport=$1
+
+timeStamp=$(echo -n $(date "+%Y-%m-%d %H:%M:%S") | shasum | cut -f 1 -d " ")
+
+mkdir -p $(pwd)/REST_Go/logs
+mkdir -p $(pwd)/REST_Go/logs/batch
+
+mainDir=$(pwd)/REST_Go
+fuzz=$(pwd)/run_fuzzing.sh
+batchDir=${mainDir}/logs/batch
+
+tools=("resttestgen")
+services=("features-service" "languagetool" "ncs" "news" "proxyprint" "restcountries" "scout-api" "scs" "erc20-rest-service" "genome-nexus" "person-controller" "problem-controller" "rest-study" "spring-batch-rest" "spring-boot-sample-app" "user-management" "cwa-verification" "market" "project-tracking-system") #"ocvn"
+
+
+log=${batchDir}/${timeStamp}.log
+
+exec 3>&1 4>&2
+trap $(exec 2>&4 1>&3) 0 1 2 3
+exec 1>$log 2>&1
+
+echo Logs:$log
+echo STARTING at $(date) >> $log
+
+i=0
+for tool in ${tools[@]}; 
+do
+    for service in ${services[@]};
+    do
+        echo RUNNING $port $tool $service >> $log
+        i=$((i+1))
+        port=$(($baseport+$i*10))
+        bash -x $fuzz $port $tool $service
+        echo RUNEND $port $tool $service >> $log
+    done
+done
+
+
