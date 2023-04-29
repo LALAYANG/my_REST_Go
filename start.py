1ae92a70904e7ea42be9f67b36d39e2dda4aa5bc
Ubuntu <azureuser@myVmamd2.hj4btkmmjwbexaxnlhsj524bgd.cx.internal.cloudapp.net>
2023-04-29 22:19:43 +0000

    fix a typo

--- start.py
+++ start.py
@@ -40,7 +40,7 @@ def run_service_tool(ports_csv):
                             lab_name = available_vm_ports[vm_port][2]
                             vm_name = available_vm_ports[vm_port][3]
                             resource_group = available_vm_ports[vm_port][4]
-                            cmd_list = ["bash", "-x", "connect_vm.sh",vm_port,vm_host,service,version,tool,service_port,times,lab_name,vm_name,resource_group,"|& tee",lab_name+"_"+vm_port +"_"+vm_name+"_"+tool+"_"+service+"_res.log"]
+                            cmd_list = ["bash", "-x", "connect_vm.sh",vm_port,vm_host,service,version,tool,service_port,times,lab_name,vm_name,resource_group," |tee",lab_name+"_"+vm_port +"_"+vm_name+"_"+tool+"_"+service+"_res.log"]
                             cmds = " ".join(cmd_list)
                             commands.append(cmds)
                             print(cmds)
