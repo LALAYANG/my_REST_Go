93745b8f0b46995a4918a8ea6e47d76cd90fb60a
Ubuntu <azureuser@myVmamd2.hj4btkmmjwbexaxnlhsj524bgd.cx.internal.cloudapp.net>
2023-04-30 03:55:47 +0000

    update

--- connect_vm.sh
+++ connect_vm.sh
@@ -41,7 +41,7 @@ print_res(){
     mkdir -p res
     sshpass -p "YANGc9" ssh -o StrictHostKeyChecking=no -p ${vm_port} -t yangc9@$vm_host '
         echo ========================RESULT=============================
-        ls /home/yangc9/my_REST_Go/REST_Go/logs/fuzzing_logs/*/*/res.csv
+        ls /home/yangc9/my_REST_Go/REST_Go/logs/fuzzing_logs/*/*/res.csv; &&
         cat /home/yangc9/my_REST_Go/REST_Go/logs/fuzzing_logs/*/*/res.csv
         echo ========================END=============================
         cd /home/yangc9/my_REST_Go/
